40b21860e3e965ca3db99c263d5c5ff8cd016d2d
Zoltán Mizsei <zmizsei@extrowerk.com>
2018-07-15 14:33:20 +0200

    README

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 /*
- * Copyright 2015 Your Name <your@email.address>
+ * Copyright 2018 Your Name <your@email.address>
  * All rights reserved. Distributed under the terms of the MIT license.
  */
 
+#include <iostream>
+#include <string>
+#include <vector>
 
-#include ".h"
-
+int main(int argc, char* argv[])
+{
+	if (argc < 2) { // We expect 2 arguments: the program name, and at least one workin folder path
+		std::cout << "AttrActive - small CLI utility to copy ID3 tags to BEFS attributes
+		std::cerr << "Usage: " << argv[0] << " Path to the work folders" << std::endl;
+		return 1;
+	}
+}
